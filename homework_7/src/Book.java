fc8184340f9291b036472237564d843578d5de18
PavloManuilenko <paulmanuilenko@gmail.com>
2016-12-01 11:22:09 +0200

    Fix of issues ib wome work 7
    Fix of issues ib wome work 7

--- homework_7/src/Book.java
+++ homework_7/src/Book.java
@@ -1,10 +1,13 @@
+/*9. Создать класс Book с полями: name (строка), bookType (перечисление), author (строка), year (целое число).
+Написать конструктор со всеми параметрами. Опишите с помощью enum тип данных для хранения литературных жанров (роман, сказка, комедия, драма, научная фантастика, детектив).
+Создать объекты класса книга (число объектов соответствует числу жанров).*/
 public class Book {
     String name;
     String author;
     int year;
-    bookType kind;
+    BookType kind;
 
-    public Book(String name, String author, int year, bookType kind){
+    public Book(String name, String author, int year, BookType kind){
         this.name = name;
         this.author = author;
         this.year = year;
@@ -12,6 +15,6 @@ public class Book {
     }
 }
 
-enum bookType {
+enum BookType {
     novel, fairyTale, comedy, drama, scienceFiction, detective;
 }
--- homework_7/src/Main.java
+++ homework_7/src/Main.java
@@ -1,69 +1,48 @@
 public class Main {
     public static void main (String[] args){
-        /*1. Составить программу, которая в зависимости от порядкового номера месяца (1, 2, ..., 12) выводит на экран количество дней в этом месяце. Рассмотреть два случая:
-        1) год не является високосным;
-        2) год может быть високосным (информация об этом вводится в аргументы при запуске программы).*/
-        System.out.println("<Task 1>\nDays in month: " + daysOfMonth((byte) 2, args));
-        /*Мастям игральных карт условно присвоены следующие порядковые номера: масти "пики" — 1, масти "трефы" — 2, масти "бубны" — 3, масти "червы" — 4,
-        а достоинству карт: "валету" — 11, "даме" — 12, "королю" — 13, "тузу" — 14 (порядковые номера карт остальных достоинств соответствуют их названиям: "шестерка", "девятка" и т. п.).
-        По заданным номеру масти m (1 <= m <=  4) и номеру достоинства карты k (6 <= k <= 14) определить полное название (масть и достоинство) соответствующей карты в виде "Дама пик", "Шестерка бубен" и т. п.*/
+       System.out.println("<Task 1>\nDays in month: " + daysOfMonth((byte) 2, args));
        System.out.println("\n<Task 2>\nCard is: " + whichCard((byte)1, (byte)12));
-       /*3. Дано целое число k (1 <= k <= 365). Определить, каким днем недели (понедельником, вторником, ..., субботой или воскресеньем) является k-й день невисокосного года, в котором 1 января:
-       а) понедельник;
-       б) i-й день недели (если 1 января - понедельник, то d=1 , если вторник - d=2, ..., если воскресенье - d= 7).*/
        System.out.println("\n<Task 3>\na) day is: " + whatDayOfWeek(15));
        System.out.println("b) day is: " + whatDayOfWeekStartWith((byte)5, 15));
-       //4. Напишите программу для перевода чисел от 1 до 10 в строковое значение. Например, для числа 1 результатом будет строка “one”.
        System.out.println("\n<Task 4>\nNum is: " + numToWord((byte)10));
-       //5. Дана строка. Написать программу, которая возвращает строку, написанную в обратном порядке.
        System.out.println("\n<Task 5>\nNum is: Reversed string: " + newStrReverse("Test of string"));
-       /*6. Дан email в виде строки. Написать программу для проверки email на валидность. email должен соответствовать условию (пример - emailaddr@domain.zone)
-       1) emailaddr > 4 символов
-       2) 1 < domain < 10
-       3) 2 <= zone < 5
-       При любом несовпадении с условием выводим в консоль сообщение с указанием на конкретную ошибку.*/
        System.out.println("\n<Task 6>\n" + validatorOfEmailAddress("Test1@google.com"));
-       /*7. Дан массив строк. Написать программу, которая:
-       а) выводит на экран значение всех строк через запятую, длина которых больше n.
-       б) выводит на экран значение всех неповторяющихся строк через запятую.*/
        String[] arr = new String[]{"Alex", "Mark", "Alex", "Paul", "Alex", "Alex", "Paul", "Mark", "Paul"};
        System.out.println("\n<Task 7>\na) " + strArrToLineA(arr));
        System.out.println("b) " + strArrToLineB(arr));
-       /*8.  Опишите с помощью enum тип данных для хранения месяцев.
-       У каждого месяца должны быть следующие свойства: номер месяца, название на английском, название на русском, количество дней.
-       Написать конструктор со всеми параметрами, а также геттеры.*/
        System.out.println("\n<Task 8>\n" + MONTHS.DECEMBER.getMonthRu());
-       /*9. Создать класс Book с полями: name (строка), bookType (перечисление), author (строка), year (целое число).
-       Написать конструктор со всеми параметрами. Опишите с помощью enum тип данных для хранения литературных жанров (роман, сказка, комедия, драма, научная фантастика, детектив).
-       Создать объекты класса книга (число объектов соответствует числу жанров).*/
-       Book bookA = new Book("NameA", "AuthorA", 1990, bookType.novel);
-       Book bookB = new Book("NameB", "AuthorB", 1995, bookType.comedy);
-       Book bookC = new Book("NameC", "AuthorC", 2000, bookType.detective);
-       Book bookD = new Book("NameD", "AuthorD", 2005, bookType.drama);
-       Book bookE = new Book("NameE", "AuthorE", 2010, bookType.fairyTale);
-       Book bookF = new Book("NameF", "AuthorF", 2015, bookType.scienceFiction);
+       Book bookA = new Book("NameA", "AuthorA", 1990, BookType.novel);
+       Book bookB = new Book("NameB", "AuthorB", 1995, BookType.comedy);
+       Book bookC = new Book("NameC", "AuthorC", 2000, BookType.detective);
+       Book bookD = new Book("NameD", "AuthorD", 2005, BookType.drama);
+       Book bookE = new Book("NameE", "AuthorE", 2010, BookType.fairyTale);
+       Book bookF = new Book("NameF", "AuthorF", 2015, BookType.scienceFiction);
     }
-
+    /*1. Составить программу, которая в зависимости от порядкового номера месяца (1, 2, ..., 12) выводит на экран количество дней в этом месяце. Рассмотреть два случая:
+    1) год не является високосным;
+    2) год может быть високосным (информация об этом вводится в аргументы при запуске программы).*/
     public static byte daysOfMonth(byte numOfMonth, String[] args){
         boolean isLeapYear = Boolean.valueOf(args[0]);
-        byte daysOfMonth;
+        //byte daysOfMonth;
         switch (numOfMonth){
-            case 1: daysOfMonth = 31; return daysOfMonth;
-            case 2: if (isLeapYear) daysOfMonth = 29; else daysOfMonth = 28; return daysOfMonth;
-            case 3: daysOfMonth = 31; return daysOfMonth;
-            case 4: daysOfMonth = 30; return daysOfMonth;
-            case 5: daysOfMonth = 31; return daysOfMonth;
-            case 6: daysOfMonth = 30; return daysOfMonth;
-            case 7: daysOfMonth = 31; return daysOfMonth;
-            case 8: daysOfMonth = 31; return daysOfMonth;
-            case 9: daysOfMonth = 30; return daysOfMonth;
-            case 10: daysOfMonth = 31; return daysOfMonth;
-            case 11: daysOfMonth = 30; return daysOfMonth;
-            case 12: daysOfMonth = 31; return daysOfMonth;
-            default: daysOfMonth = 0; return daysOfMonth;
+            case 1: return 31;
+            case 2: if (isLeapYear) return 29; else return 28;
+            case 3: return 31;
+            case 4: return 30;
+            case 5: return 31;
+            case 6: return 30;
+            case 7: return 31;
+            case 8: return 31;
+            case 9: return 30;
+            case 10: return 31;
+            case 11: return 30;
+            case 12: return 31;
+            default: return 0;
         }
     }
-
+    /*Мастям игральных карт условно присвоены следующие порядковые номера: масти "пики" — 1, масти "трефы" — 2, масти "бубны" — 3, масти "червы" — 4,
+    а достоинству карт: "валету" — 11, "даме" — 12, "королю" — 13, "тузу" — 14 (порядковые номера карт остальных достоинств соответствуют их названиям: "шестерка", "девятка" и т. п.).
+    По заданным номеру масти m (1 <= m <=  4) и номеру достоинства карты k (6 <= k <= 14) определить полное название (масть и достоинство) соответствующей карты в виде "Дама пик", "Шестерка бубен" и т. п.*/
     public static String whichCard(byte m, byte k){
         String whichCard = "";
         switch (k){
@@ -86,7 +65,9 @@ public class Main {
             default: whichCard += "[Can't recognize the suit]"; return whichCard;
         }
     }
-
+    /*3. Дано целое число k (1 <= k <= 365). Определить, каким днем недели (понедельником, вторником, ..., субботой или воскресеньем) является k-й день невисокосного года, в котором 1 января:
+    а) понедельник;
+    б) i-й день недели (если 1 января - понедельник, то d=1 , если вторник - d=2, ..., если воскресенье - d= 7).*/
     public static String whatDayOfWeek(int k){
         String whatDayOfWeek = "";
         while (k > 7) {
@@ -123,7 +104,7 @@ public class Main {
             }
         return whatDayOfWeek;
     }
-
+    //4. Напишите программу для перевода чисел от 1 до 10 в строковое значение. Например, для числа 1 результатом будет строка “one”.//5. Дана строка. Написать программу, которая возвращает строку, написанную в обратном порядке.
     public static String numToWord(byte nume){
         String numToWord = "";
         switch (nume){
@@ -140,7 +121,7 @@ public class Main {
             default: numToWord = "[Can't recognize entered number]"; return numToWord;
         }
     }
-
+    //5. Дана строка. Написать программу, которая возвращает строку, написанную в обратном порядке.
     public static String newStrReverse(String str){
         StringBuilder builder = new StringBuilder();
         char[] strArr = str.toCharArray();
@@ -150,7 +131,11 @@ public class Main {
         String newStrReverse = builder.toString();
         return newStrReverse;
     }
-
+    /*6. Дан email в виде строки. Написать программу для проверки email на валидность. email должен соответствовать условию (пример - emailaddr@domain.zone)
+    1) emailaddr > 4 символов
+    2) 1 < domain < 10
+    3) 2 <= zone < 5
+    При любом несовпадении с условием выводим в консоль сообщение с указанием на конкретную ошибку.*/
     public static String validatorOfEmailAddress(String email){
         String validatorOfEmailAddress = "";
         String emailaddr = email.substring(0, email.indexOf("@"));
@@ -161,7 +146,9 @@ public class Main {
         else if (zone.length() <= 1 || zone.length() > 5) return validatorOfEmailAddress = "Incorrect zone, please check and retry. ";
         else return validatorOfEmailAddress = "The email address is correct";
     }
-
+    /*7. Дан массив строк. Написать программу, которая:
+    а) выводит на экран значение всех строк через запятую, длина которых больше n.
+    б) выводит на экран значение всех неповторяющихся строк через запятую.*/
     public static String strArrToLineA(String[] steArr){
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < steArr.length; i++){
@@ -185,7 +172,9 @@ public class Main {
         if (strArrToLine.endsWith(", ")) return strArrToLine.substring(0, strArrToLine.length()-2);
         else return strArrToLine;
     }
-
+    /*8.  Опишите с помощью enum тип данных для хранения месяцев.
+    У каждого месяца должны быть следующие свойства: номер месяца, название на английском, название на русском, количество дней.
+    Написать конструктор со всеми параметрами, а также геттеры.*/
     public enum MONTHS {
         JANUARY(1,"January", "Январь", 31),
         FABRUARY(2,"Fabruary", "Февраль", 28),
