66329b5b8fc55894337e35e188fce51d0fe6410f
PavloManuilenko <paulmanuilenko@gmail.com>
2017-02-16 11:44:40 +0200

    Add new tests to hw_23
    Add new tests to hw_23

--- homework_23/src/ArraysTest.java
+++ homework_23/src/ArraysTest.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
-import sun.plugin.javascript.navig.Array;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Collections;
 import static org.junit.Assert.*;
 
 public class ArraysTest {
@@ -76,7 +74,100 @@ public class ArraysTest {
     }
 
     @Test
-    public void testOfArraysCloneViaCopyOf() {
+    public void testOfIntArrayCloneViaCopyOf() {
+        //given
+        int[] arr1 = new int[]{10, 20, 30, 40, 50};
+
+        //when
+        int[] arr2 = Arrays.copyOf(arr1, 5);
+
+        String expected = Arrays.toString(arr1);
+        String actual = Arrays.toString(arr2);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfDoubleArrayCloneViaCopyOf() {
+        //given
+        double[] arr1 = new double[]{10.5, 20.2, 30.8, 40.72, 50.5};
+
+        //when
+        double[] arr2 = Arrays.copyOf(arr1, 5);
+
+        String expected = Arrays.toString(arr1);
+        String actual = Arrays.toString(arr2);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfStringArrayCloneViaCopyOf() {
+        //given
+        String[] arr1 = new String[]{"Test", "1", "2", "3"};
+
+        //when
+        String[] arr2 = Arrays.copyOf(arr1, 4);
+
+        //then
+        assertEquals(arr1, arr2);
+    }
+
+    @Test
+    public void testOfIntArrayCopyOfFullViaCopyOfRange() {
+        //given
+        int[] arr1 = new int[]{10, 20, 30, 40, 50};
+
+        //when
+        int[] arr2 = Arrays.copyOfRange(arr1, 0, 5);
+
+        String expected = Arrays.toString(arr1);
+        String actual = Arrays.toString(arr2);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfIntArrayCopyOfBeginViaCopyOfRange() {
+        //given
+        int[] arr1 = new int[]{10, 20, 30, 40, 50};
+
+        //when
+        int[] arr2 = Arrays.copyOfRange(arr1, 0, 3);
+
+        String expected = "[10, 20, 30]";
+        String actual = Arrays.toString(arr2);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfIntArrayCopyOfEndViaCopyOfRange() {
+        //given
+        int[] arr1 = new int[]{10, 20, 30, 40, 50};
+
+        //when
+        int[] arr2 = Arrays.copyOfRange(arr1, 2, 5);
+
+        String expected = "[30, 40, 50]";
+        String actual = Arrays.toString(arr2);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testIllegalArgumentExceptionUsingCopyOfRange() throws IllegalArgumentException {
+        int[] arr1 = new int[]{10, 20, 30};
+        int[] arr2 = Arrays.copyOfRange(arr1, 3, 1);
+    }
+
+    @Test
+    public void testOfIntSorting() {
         //given
         ArrayList<Integer> numbers = new ArrayList<>(3);
 
@@ -85,18 +176,50 @@ public class ArraysTest {
         }
 
         //when
-        Integer[] temp = new Integer[numbers.size()];
-        temp = numbers.toArray(temp);
-
-        Integer[] numbersNew = Arrays.copyOf(temp, temp.length);
-
-        String expected = numbers.toString();
-        String actual = numbersNew.toString();
-
+        Collections.sort(numbers);
+        String expected = "[1, 2, 3]";
+        String actual = numbers.toString();
 
         //then
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testOfBooleanSorting() {
+        //given
+        ArrayList<Boolean> booleans = new ArrayList<>(2);
+
+            booleans.add(true);
+            booleans.add(false);
+            booleans.add(false);
+            booleans.add(true);
+
+        //when
+        Collections.sort(booleans);
+        String expected = "[false, false, true, true]";
+        String actual = booleans.toString();
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfStringCollectionSorting() {
+        //given
+        ArrayList<String> words = new ArrayList<>(2);
+
+        words.add("C");
+        words.add("D");
+        words.add("A");
+        words.add("B");
+
+        //when
+        Collections.sort(words);
+        String expected = "[A, B, C, D]";
+        String actual = words.toString();
+
+        //then
+        assertEquals(expected, actual);
+    }
 
 }
