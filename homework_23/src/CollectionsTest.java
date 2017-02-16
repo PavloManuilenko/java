21b634264209fc343290160dfddbf7221285e8c3
Pavlo <pavlomanuilenko@gmail.com>
2017-02-16 20:06:55 +0200

    Add first test into CollectionsTest of hw_23

--- homework_23/src/CollectionsTest.java
+++ homework_23/src/CollectionsTest.java
@@ -1,5 +1,38 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CollectionsTest {
+
+    @Test
+    public void testOfStringListCopy() {
+        //given
+        List<String> listA = new ArrayList<String>(3);
+        List<String> listB = new ArrayList<String>(3);
+
+        //when
+        listA.add("one");
+        listA.add("two");
+        listA.add("three");
+
+        listB.add("1");
+        listB.add("2");
+        listB.add("3");
+
+        Collections.copy(listB, listA);
+
+        //then
+        assertEquals(listA, listB);
+
+
+
+
+
+
+    }
 
 }
