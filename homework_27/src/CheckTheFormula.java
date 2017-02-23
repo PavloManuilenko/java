d6185782f44bac2e86b58babcd5a8fd9380227a9
Pavlo <pavlomanuilenko@gmail.com>
2017-02-23 18:38:29 +0200

    add to hw_27 task 2

--- homework_27/src/CheckTheFormula.java
+++ homework_27/src/CheckTheFormula.java
@@ -2,41 +2,29 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Arrays;
 
 public class CheckTheFormula {
 
     List<String> strings = new ArrayList<>(1);
 
     public void searchForMissingParentheses(String inputFile, String outputFile) {
-
         //read from the input-file
         String lineText;
-        BufferedReader reader;
-        FileWriter writer;
-        try {
-            reader = new BufferedReader(new FileReader(inputFile));
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             while ((lineText = reader.readLine()) != null) {
                 strings.add(lineText + "\r\n");
             }
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //write into the output-file
-        try {
-            writer = new FileWriter(outputFile);
-
+        //write to the output-file
+        try (FileWriter writer = new FileWriter(outputFile)) {
             Iterator<String> iterator = strings.iterator();
-
             while (iterator.hasNext()) {
                 String tempStr = iterator.next();
                 writer.write(tempStr);
                 writer.append(checker(tempStr));
             }
-            writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,24 +33,31 @@ public class CheckTheFormula {
 
     //logic of verifying a formula
     public String checker(String sourceStr) {
-        String checkedString = new String();
         char[] strCopy = sourceStr.toCharArray();
         StringBuilder builder = new StringBuilder();
+
+        removePairOfParentheses(strCopy, '(', ')');
+        removePairOfParentheses(strCopy, ')', '(');
+
         for (int i = 0; i < strCopy.length; i++) {
-            if (strCopy[i] == '(') {
-                for (int j = i + 1; j < strCopy.length; j++) {
-                    if (strCopy[j] == ')') {
-                        strCopy[i] = ' ';
-                        strCopy[j] = ' ';
+            if (strCopy[i] == '(' || strCopy[i] == ')') builder.append('^');
+            else builder.append(' ');
+        }
+        return builder.toString() + "\r\n";
+    }
+
+    public void removePairOfParentheses(char[] sourceStr, char parentheses1, char parentheses2) {
+        for (int i = 0; i < sourceStr.length; i++) {
+            if (sourceStr[i] == parentheses1) {
+                for (int j = i + 1; j < sourceStr.length; j++) {
+                    if (sourceStr[j] == parentheses2) {
+                        sourceStr[i] = ' ';
+                        sourceStr[j] = ' ';
                         break;
                     }
                 }
             }
         }
-        for (int i = 0; i < strCopy.length; i++) {
-            if (strCopy[i] == '(') builder.append('^');
-            else builder.append(strCopy[i]);
-        }
-        return checkedString = builder.toString() + "\r\n";
     }
+
 }
